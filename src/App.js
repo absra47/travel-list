6c32f9f1396338fe3b0054e87ba5224eadaf1b8c
absra47 <absragirma630@gmail.com>
2023-11-09 13:39:34 +0300

    trip-project 101

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Logo />
+      <Form />
+      <PakingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you want for your trip?</h3>
+    </div>
+  );
+}
+function PakingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer>
+      <em> You have X items on your list , and you already packed X (X%) </em>
+    </footer>
+  );
+}
